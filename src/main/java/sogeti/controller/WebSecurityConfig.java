d0d391e572b94b9d88b44e4e0f83e6f8cda74b5a
Ilyasse <ilyasse.tissafiidrissi@gmail.com>
2020-04-24 14:15:56 +0200

    modified mapping and started login auth

---
+++ src/main/java/sogeti/controller/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package sogeti.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                    .antMatchers("/", "/register").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .and()
+                .logout()
+                    .permitAll();
+    }
+    @Bean
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                    .username("mail")
+                    .password("password")
+                    .roles("simple_user")
+                    .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
