bb8b6344617844e723dec9031e8b79f960c47f11
Florian <florian.binder@wanadoo.fr>
2020-04-27 15:26:35 +0200

    push du commit...

--- src/main/java/sogeti/controller/RegisterPatientProtocol.java
+++ src/main/java/sogeti/controller/RegisterPatientProtocol.java
@@ -1,4 +1,25 @@
 package sogeti.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class RegisterPatientProtocol {
+	
+	@GetMapping("/HealthCarePatientRegisterForm.html")
+	public ModelAndView getPatientRegisterForm() {
+		
+		String viewName = "patientRegisterForm";
+		
+		Map<String, Object> model = new HashMap<String, Object>(); 
+		
+		model.put("prenom", "Florian");
+		
+		return new ModelAndView(viewName, model);
+	}
+	
 }
